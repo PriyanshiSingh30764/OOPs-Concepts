1788b125305fad02a3cca27aa3f8bc7890ac3b1f
Priyanshi singh <priyanshi.singh@unthinkable.co>
2024-01-19 13:55:11 +0530

    Basic class and Obj

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ namespace FirstClass
+ {
+
+ public class Introduction
+ {
+    //  string Name;
+    //  int age;
+
+     public void intro(string name,int age)
+     {
+       Console.WriteLine($"How are you {name} with an age of {age}");
+     }
+ }
+
+public class FirstClassInOpps
+{
+public static void Main()
+{
+   Console.WriteLine("Hey!! what's your name");
+   string name = Console.ReadLine();
+   Introduction fcObj = new Introduction();
+   fcObj.intro(name , 23);
+}
+}
+ }
